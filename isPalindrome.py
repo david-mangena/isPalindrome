6b7427cbe6cb63f3891f3251fa4a8547c3b23890
David <dvdmangena@gmail.com>
2021-01-24 17:48:20 +0200

    modifying readMe file with test data

--- isPalindrome.py
+++ isPalindrome.py
@@ -2,18 +2,18 @@
 #function which return reverse of a string
 def isPalindrome(string):
 
-    # Checking if both string are
-    # equal or not
+    # Checking if both string are equal or not
     return string == string[::-1]
 
 #Enter string
 inputString = raw_input("Enter String: ")
 
 #lowercase input string
-answer = isPalindrome(inputString.lower())
-print answer
-# outcome
+answer = isPalindrome(inputString)
+
+# print outcome
 if answer:
     print("It's Palindrome")
 else:
     print("It's not Palindrome")
+

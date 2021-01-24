8537de2178c46ed16b9b554cd6913ae9217d172b
David <dvdmangena@gmail.com>
2021-01-24 18:11:52 +0200

    remove punctuations and white spaces from string

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,14 +1,19 @@
 
 #function which return reverse of a string
+import re
+
 def isPalindrome(string):
 
+    # Removing punctuations, white spaces and lowercase string
+    res = re.sub(r'[^\w\s]', '', string.replace(' ','').lower())
+
     # Checking if both string are equal or not
-    return string == string[::-1]
+    return res == res[::-1]
 
 #Enter string
 inputString = raw_input("Enter String: ")
 
-#lowercase input string
+#lowercase and remove  input string
 answer = isPalindrome(inputString)
 
 # print outcome
